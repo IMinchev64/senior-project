04ad24b1e75a5317332dd54eb2be8bd525e2c9b4
Ivan Minchev <ivanminchev9@gmail.com>
2023-02-09 18:30:00 +0200

    Setup repository
    Initial commit to setup the local repository
    
    Testing Done: project built via mvn clean install

---
+++ src/test/java/edu/aubg/ics/IcsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.aubg.ics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
