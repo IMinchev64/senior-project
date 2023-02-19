452ecdc12f4507d60b7651237e0c459c33cfc70c
Ivan Minchev <ivanminchev9@gmail.com>
2023-02-19 21:01:51 +0200

    Add DAO directory and minor fixes
    Add DAO directory for daos to connect to DB.
    Also add minor fixes tied to the /images endpoint

--- src/main/java/edu/aubg/ics/IcsApplication.java
+++ src/main/java/edu/aubg/ics/IcsApplication.java
@@ -3,10 +3,12 @@ package edu.aubg.ics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class IcsApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(IcsApplication.class, args);
 	}
 
--- src/main/java/edu/aubg/ics/controller/ImageController.java
+++ src/main/java/edu/aubg/ics/controller/ImageController.java
@@ -12,8 +12,8 @@ public class ImageController {
     @Autowired
     private Connector imaggaConnector;
 
-    @PostMapping("/images")
-    public ResponseEntity<String> getImageTags(@RequestBody String imageUrl) throws IOException {
+    @GetMapping("/")
+    public ResponseEntity<String> getImageTags(@RequestParam String imageUrl) throws IOException {
         imaggaConnector.connect(imageUrl);
         return ResponseEntity.ok("Request submitted");
     }
